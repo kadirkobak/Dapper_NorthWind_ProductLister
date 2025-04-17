e749c1c334335c23935adaa2be7bfaf5195542c5
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-17 12:17:38 +0300

    Create category dto added.

---
+++ Dapper_NorthWind_ProductLister/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_NorthWind_ProductLister.Dtos.CategoryDtos
+{
+    public  class CreateCategoryDto
+    {
+     
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
