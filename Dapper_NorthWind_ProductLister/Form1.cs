4dc1eccccd114a7369b6107aa4c36ec8d1f9d4b5
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-17 15:03:35 +0300

    Lister added.

--- Dapper_NorthWind_ProductLister/Form1.cs
+++ Dapper_NorthWind_ProductLister/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Dapper;
+using Dapper_NorthWind_ProductLister.Dtos.CategoryDtos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,14 +19,18 @@ namespace Dapper_NorthWind_ProductLister
         {
             InitializeComponent();
         }
+        
+        SqlConnection connection = new SqlConnection("Server=1LP021\\SQLEXPRESS;initial catalog=DbDapperNorthWind;" +
+            "integrated security=true;TrustServerCertificate=true");
 
-        private void btnCategoryList_Click(object sender, EventArgs e)
+
+        private async void btnCategoryList_Click(object sender, EventArgs e)
         {
             string query = "SELECT * FROM Categories";
-            var connection = "Server=1LP021\\SQLEXPRESS;initial catalog=DbDapperNorthWind;" +
-          "integrated security=true;TrustServerCertificate=true";
 
-            //var values =
+
+            var values = await connection.QueryAsync<ResultCategoryDto>(query);
+            dataGridView1.DataSource = values.ToList();
         }
     }
 }
